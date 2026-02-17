56dcab64ffe6319263b6b175cf653734036ec450
Yujing <wangyujing0810@gmail.com>
2026-02-16 21:17:46 -0800

    create the WebGLGlobalBridge script and prefab

---
+++ Assets/Scripts/WebGLGlobalBridge.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+/// <summary>
+/// Global bridge that JavaScript can call, which then forwards to the actual WebGLTextBridge instance
+/// This solves the nested GameObject problem
+/// </summary>
+public class WebGLGlobalBridge : MonoBehaviour
+{
+    public static WebGLGlobalBridge Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            Debug.Log("[WebGLGlobalBridge] Global bridge created. JavaScript should call this GameObject: " + gameObject.name);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Called from JavaScript - forwards to WebGLTextBridge.Instance
+    public void InjectText(string text)
+    {
+        Debug.Log("[WebGLGlobalBridge] InjectText called, forwarding to WebGLTextBridge.Instance");
+        
+        if (WebGLTextBridge.Instance != null)
+        {
+            WebGLTextBridge.Instance.InjectText(text);
+        }
+        else
+        {
+            Debug.LogError("[WebGLGlobalBridge] WebGLTextBridge.Instance is null! Make sure WebGLTextBridge component exists in scene.");
+        }
+    }
+
+    // Called from JavaScript - forwards to WebGLTextBridge.Instance
+    public void UpdateVoiceStatus(string status)
+    {
+        if (WebGLTextBridge.Instance != null)
+        {
+            WebGLTextBridge.Instance.UpdateVoiceStatus(status);
+        }
+    }
+}
